45bbcbafbfeaf8b49aeb1d9f2ef5208d9ecc08f9
T.J. Park <sincerity0113@gmail.com>
2019-09-06 10:47:21 -0400

    Updates

---
+++ precache-manifest.c4d42552659d296187902536ab6fc9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6510bfa0ea3dfe323a1bba27144fbe42",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "bbede2ae508663eacc03",
+    "url": "/clickygame/static/css/main.a8d9ce06.chunk.css"
+  },
+  {
+    "revision": "c96024e5b3e5ca9f421f",
+    "url": "/clickygame/static/js/2.be6850b8.chunk.js"
+  },
+  {
+    "revision": "bbede2ae508663eacc03",
+    "url": "/clickygame/static/js/main.81f7ac6e.chunk.js"
+  },
+  {
+    "revision": "0767f4cbf998e7d82d82",
+    "url": "/clickygame/static/js/runtime~main.832da4da.js"
+  }
+]);
